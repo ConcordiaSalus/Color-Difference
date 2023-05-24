9e51b3cddfff4ba288a34d395d2246cb577bc98a
dcaruelle <david.caruelle@orange.fr>
2023-05-24 20:08:03 +0200

    new file:   Delta_E_CIE.py

---
+++ Delta_E_CIE.py
@@ -0,0 +1,9 @@
+
+import math
+
+def Delta_E_CIE ( CIE_L1, CIE_a1, CIE_b1, CIE_L2, CIE_a2, CIE_b2 ):
+    Delta_E = math.sqrt( ( ( CIE_L1 - CIE_L2 ) ** 2 )
+               + ( ( CIE_a1 - CIE_a2 ) ** 2 )
+               + ( ( CIE_b1 - CIE_b2 ) ** 2 ) )
+    
+    return Delta_E

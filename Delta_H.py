7492bb238975cc82624d0d0bd50610f0913dd2fb
ConcordiaSalus <david.caruelle@orange.fr>
2023-05-27 23:55:10 +0200

    modified:   Delta_H.py

--- Delta_H.py
+++ Delta_H.py
@@ -1,6 +1,7 @@
 
 import math
 
+
 def Delta_H ( CIE_a1, CIE_b1, CIE_a2, CIE_b2 ):
     xDE =  math.sqrt( ( CIE_a2 ** 2 ) + ( CIE_b2 ** 2 ) ) - math.sqrt( ( CIE_a1 ** 2 ) + ( CIE_b1 ** 2 ) )
     Delta_H = math.sqrt( ( CIE_a2 - CIE_a1 ) ** 2 + ( CIE_b2 - CIE_b1 ) ** 2 - ( xDE ** 2 ) )
